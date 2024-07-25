1c92623e5aff162cae29f6a923955b5af8abfe0e
nut3219 <wong1904@hotmail.com>
2024-07-25 14:00:06 +0800

    add files to lfs

---
+++ installers/scripts/offline-install-docker-k3d-kubectl.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ade13d5bc75aca13bd0942fc917fb97c9e6b88b44b597ece2500c6520335ffd9
+size 1314
---
+++ installers/scripts/online-download-installers.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:31d0d61ce60101879f16393590c4bdbfbd8d3f905558bde8b61d75fdcf844bf9
+size 2885
